96c40062c65d1b531863acac390eb8b82666a2ec
Noah Bolger <ncbolger@uwaterloo.ca>
2020-01-21 13:57:36 -0500

    write to output file, commenting

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import sys
 import csv
 import random
 
+# parse command line params
 command_inputs = list(sys.argv)
 input_data = command_inputs[1]
 json_data = command_inputs[2]
 
+# retrieve json data
 with open(json_data) as hyperparams:
     params = json.load(hyperparams)
     learning_rate = params['learning rate']
     iterations = params['num iter']
 
+# retrieve training data
 x_vectors = []
 y_labels = []
 with open(input_data) as data:
@@ -22,15 +25,13 @@ with open(input_data) as data:
         x_vectors.append(x_data)
         y_labels.append(float(row[-1]))
 
+# vector and matrix handling
 x_matrix = np.matrix(x_vectors)
 x_transpose = x_matrix.T
 y_vector = np.matrix(y_labels).T
 
+# retrieve the resulting weights of least squares regression
 weights = np.linalg.inv(x_transpose*x_matrix) * x_transpose * y_vector
-print(weights)
-#TODO: write to output file
-
-#TODO: stochastic gradient descent
 
 def stoch_grad_descent(iters, learn_rate, x_data, y_data):
     # initialize weights vector as vector of zeros
@@ -38,6 +39,7 @@ def stoch_grad_descent(iters, learn_rate, x_data, y_data):
     y_data = np.array(y_data)
     w_i = np.zeros((x_data.shape[1], 1))
 
+    # change iterations
     iters = 500000
 
     while iters > 0:
@@ -48,5 +50,17 @@ def stoch_grad_descent(iters, learn_rate, x_data, y_data):
         iters -= 1
     return w_i
 
+# retrieve the results of stochastic gradient descent
 sgd = stoch_grad_descent(iterations, learning_rate, x_vectors, y_labels)
-print(sgd)
+
+# open and write analytic/sgw results to output file
+with open(input_data.replace('.in', '.out'), 'w') as f:
+    for i in range(0, weights.size):
+        f.write(str(float(weights[i])))
+        f.write('\n')
+
+    f.write('\n')
+
+    for j in range(0, sgd.size):
+        f.write(str(float(sgd[j])))
+        f.write('\n')
